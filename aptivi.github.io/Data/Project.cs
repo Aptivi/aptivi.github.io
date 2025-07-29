8a1af5b3c875f714d1aab98012ca26f01f47a5f9
Aptivi <eoflaoevicecity@gmail.com>
2025-07-29 12:07:09 +0300

    add - Added GitLab links
    ---
    
    We've added GitLab links and marked GitHub ones as mirrors.
    
    ---
    
    Type: add
    Breaking: False
    Doc Required: False
    Backport Required: False
    Part: 1/1

--- aptivi.github.io/Data/Project.cs
+++ aptivi.github.io/Data/Project.cs
@@ -5,6 +5,7 @@ namespace aptivi.github.io.Data
         public string ProjectName { get; set; }
         public string Description { get; set; }
         public string ProjectSlug { get; set; }
+        public string ProjectGLSlug { get; set; }
         public string ProjectGBSlug { get; set; }
         public string[] NuGetPackages { get; set; }
         public string ProjectImageName { get; set; }
